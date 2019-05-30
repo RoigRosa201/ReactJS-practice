4d09b923c31183e819f791393e59b2dbcbf40539
RoigRosa201 <roigrosa88@gmail.com>
2019-05-30 17:39:28 -0400

    added dynamic component that is reusable props.name and props.age

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ class App extends Component {
     <div className="App">
       <h1> Hi, I am a react app.</h1>
       <p>This is really working!</p>
-      <Person /> 
-      <Person />
+      <Person name="Roig" age="29" />
+      <Person name="Mia" age="25" > My Hobbies: Anime </Person>
       </div>
   );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
